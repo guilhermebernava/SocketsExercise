e2d24e1b56caa46b568996820a22dbdb88a107ac
Guilherme <guilhermebernava00@gmail.com>
2021-11-05 14:16:44 -0300

    SOCKET SERVER AND CLIENT CREATE AND WORKING BASICALLY

---
+++ SocketExercise/Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Loopback, 9000);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("PRESS <ENTER> TO CONNECT WITH SERVER");
+            Console.ReadLine();
+            try
+            {
+                socket.Connect(ipep);
+                Console.WriteLine("CONNECTION SUCESSFULL");
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            byte[] bufferSend = new byte[1024];
+            byte[] bufferReceived = new byte[1024];
+            while (socket.Connected)
+            {
+                Console.Write("WRITE: ");
+                var textSend = Console.ReadLine();
+                bufferSend = Encoding.UTF8.GetBytes(textSend);
+                socket.Send(bufferSend);
+                Array.Clear(bufferSend, 0, bufferSend.Length);
+
+                socket.Receive(bufferReceived);
+                var textReceived = Encoding.UTF8.GetString(bufferReceived);
+                Console.Write($"SERVER: {textReceived}"); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SocketExercise/Server/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Loopback, 9000);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("WAITING FOR A CLIENT......");
+            try
+            {
+                socket.Bind(ipep);
+                socket.Listen(2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            var client = socket.Accept();
+            Console.WriteLine("CLIENT ACCEPT!");
+            byte[] bufferSend = new byte[1024];
+            byte[] bufferReceived = new byte[1024];
+
+            while (client.Connected)
+            {
+                 client.Receive(bufferReceived);
+
+                 string textReceived = Encoding.UTF8.GetString(bufferReceived);
+
+                Console.Write($"Client: {textReceived}");
+
+                Array.Clear(bufferReceived, 0, bufferReceived.Length);
+                Console.Write("WRITE: ");
+                var textSend = Console.ReadLine();
+                if(textSend == "exit")
+                {
+                    break;
+                }
+                bufferSend = Encoding.UTF8.GetBytes(textSend);
+                client.Send(bufferSend);
+                Array.Clear(bufferSend, 0, bufferSend.Length);
+            }
+
+            Console.WriteLine("CONNECTION IS FINISHED!");
+            Console.ReadLine();
+        }
+    }
+}
